8fd106c0a458fbc1e41dd2392267d0e9b0ea5f11
Moritz Fink <moritz.fink@accenture.com>
2022-09-29 22:24:49 +0200

    Add Authenticator

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "flutteramplifytodo6410f4ce": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_authenticator/amplify_authenticator.dart';
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_amplify_todo/page/todo_list_page.dart';
@@ -39,12 +40,15 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Amplify ToDo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    return Authenticator(
+      child: MaterialApp(
+        builder: Authenticator.builder(),
+        title: 'Flutter Amplify ToDo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const ToDoListPage(),
       ),
-      home: const ToDoListPage(),
     );
   }
 }
