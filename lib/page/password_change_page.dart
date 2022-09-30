33de0e1f3f3a6ab918ee657523e3bf4c6730e6d1
Moritz Fink <moritz.fink@accenture.com>
2022-09-30 11:42:44 +0200

    Add page for password change

---
+++ lib/page/password_change_page.dart
@@ -0,0 +1,97 @@
+import 'package:amplify_flutter/amplify_flutter.dart';
+import 'package:flutter/material.dart';
+
+class PasswordChangePage extends StatefulWidget {
+  const PasswordChangePage({Key? key}) : super(key: key);
+
+  @override
+  State<PasswordChangePage> createState() => _PasswordChangePageState();
+}
+
+class _PasswordChangePageState extends State<PasswordChangePage> {
+  final _formKey = GlobalKey<FormState>();
+  late TextEditingController _oldPasswordController;
+  late TextEditingController _newPasswordController;
+
+  @override
+  void initState() {
+    super.initState();
+    _oldPasswordController = TextEditingController(text: '');
+    _newPasswordController = TextEditingController(text: '');
+  }
+
+  @override
+  void dispose() {
+    _oldPasswordController.dispose();
+    _newPasswordController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Change Password'),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(32.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextFormField(
+                  controller: _oldPasswordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    labelText: 'Old Password',
+                  ),
+                  validator: _validatePassword,
+                ),
+                TextFormField(
+                  controller: _newPasswordController,
+                  obscureText: true,
+                  decoration: const InputDecoration(
+                    labelText: 'New Password',
+                  ),
+                  validator: _validatePassword,
+                ),
+                ElevatedButton(
+                  onPressed: _changePassword,
+                  child: const Text('Change Password'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  String? _validatePassword(String? password) {
+    if (password == null) {
+      return 'Password cannot be empty';
+    }
+    if (!RegExp(r"^[\S]+.*[\S]+$").hasMatch(password.trim())) {
+      return 'Invalid password';
+    }
+    return null;
+  }
+
+  Future<void> _changePassword() async {
+    if (_formKey.currentState!.validate()) {
+      try {
+        await Amplify.Auth.updatePassword(
+          oldPassword: _oldPasswordController.text.trim(),
+          newPassword: _newPasswordController.text.trim(),
+        ).then((_) => Navigator.pop(context));
+      } on AmplifyException catch (e) {
+        safePrint(e.message);
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text(e.message),
+        ));
+      }
+    }
+  }
+}
--- lib/page/todo_list_page.dart
+++ lib/page/todo_list_page.dart
@@ -1,7 +1,9 @@
 import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_amplify_todo/page/password_change_page.dart';
 
 enum MenuOptions {
+  changePassword,
   signOut,
 }
 
@@ -24,12 +26,19 @@ class _ToDoListPageState extends State<ToDoListPage> {
             icon: const Icon(Icons.person),
             itemBuilder: (context) => [
               const PopupMenuItem(
+                value: MenuOptions.changePassword,
+                child: Text('Change Password'),
+              ),
+              const PopupMenuItem(
                 value: MenuOptions.signOut,
                 child: Text('Sign Out'),
               ),
             ],
             onSelected: (value) {
               switch(value) {
+                case MenuOptions.changePassword:
+                  _changePassword();
+                  break;
                 case MenuOptions.signOut:
                   _signOut();
                   break;
@@ -42,6 +51,14 @@ class _ToDoListPageState extends State<ToDoListPage> {
       ),
       body: const Center(
         child: Text('Congratulations, there is nothing left to do!'),
+      ),
+    );
+  }
+
+  _changePassword() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const PasswordChangePage(),
       ),
     );
   }
